78d40580857f24c2276fac72020b9e0b91975c17
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2019-10-28 16:49:06 +0300

    fixes

--- client/src/app/components/pages/accept-page/accept-page.component.ts
+++ client/src/app/components/pages/accept-page/accept-page.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-accept-board',
   templateUrl: './accept-page.component.html',
   styleUrls: ['./accept-page.component.less']
 })
-export class AcceptPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class AcceptPageComponent {}
